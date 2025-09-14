03c7201a4008ff36f76866ee9cf6b2675a866ff7
洺暄 <guaibaobao2006@hotmail.com>
2025-09-14 14:01:25 +0800

    remove collectstatic command for now

--- start.sh
+++ start.sh
@@ -5,9 +5,6 @@
 echo "Applying database migrations..."
 python manage.py migrate
 
-# 收集静态文件（可选，如果有静态文件）
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
 
 # 启动Gunicorn
 echo "Starting Gunicorn server..."
